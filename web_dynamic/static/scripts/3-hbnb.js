d05e6aeb965dc0ffb8e23e28316b15b317793d99
ThibaudP <t.poncin@gmail.com>
2021-05-13 23:19:00 +0000

    feat: move place fetching to frontend

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,60 @@
+const checkAmenities = function () {
+  const checkedAmenities = {};
+  let displayAmenities = [];
+  $('.amenities .popover input').change(function () {
+    if ($(this).prop('checked')) {
+      checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete checkedAmenities[$(this).attr('data-id')];
+    }
+    displayAmenities = Object.values(checkedAmenities).sort();
+    $('.amenities h4').text(displayAmenities.join(', '));
+  });
+};
+
+const checkApiStatus = function () {
+  $.getJSON('http://localhost:5001/api/v1/status', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+};
+
+const fetchPlaces = function () {
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: (data) => {
+      data.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+      });
+      for (const place of data) {
+        const article = `
+        <article>
+          <h2>${place.name}</h2>
+          <div class="price_by_night">$${place.price_by_night}</div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guests</div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+        $('section.places').append(article);
+      }
+    }
+  }
+  );
+};
+
+$(() => {
+  checkAmenities();
+  checkApiStatus();
+  fetchPlaces();
+});
